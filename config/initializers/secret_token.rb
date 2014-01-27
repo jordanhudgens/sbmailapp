6e5c03edc598b725c7725a40aba1325945ecc998
Jordan Hudgens <jordan@wow.com>
2014-01-27 11:14:34 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sbmailer::Application.config.secret_key_base = 'cdb57923d06781f803c4f2a088474a488d46fc8a04ee41b5541895e6aaa262f7dda07d86a7dba768bbe43085b48867a8e4b8408854e498418f117743d526b4e6'
